7dd4fcbd5cb55de19d862f0fc08fc5f2d4fd10bd
toanftraanf <tranthetoan2003@gmail.com>
2026-01-02 17:43:38 +0700

    FIX<toantt>: use public stable API import path for usePathname

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import {usePathname} from "next/dist/client/components/navigation";
+import {usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
 
 const Header = () => {
